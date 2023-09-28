a8c31131ee3ce7f60498ef0dc51c0c5c548f76b3
CarelessOutcast <tapiacarlos32@gmail.com>
2023-09-27 20:34:29 -0500

    Fix migrate.sh for windows

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo "Make sure you have docker installed!"
 echo "Starting development env on localhost:8000 ..."
-docker compose -f docker-compose.yaml up --build -d 
+docker compose -f docker-compose.yaml up -d 
 
