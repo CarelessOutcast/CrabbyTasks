f5514e1fe7869a65490f82dd70e49c45f31daba8
CarelessOutcast <tapiacarlos32@gmail.com>
2023-10-13 20:07:29 -0500

    Fix settings and entrypoint docker

--- web/crabbytasks/settings.py
+++ web/crabbytasks/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',
     'main',
 ]
 
@@ -157,9 +156,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-COMPRESSOR_ROOT = BASE_DIR / 'static'
-COMPRESS_ENABLED = True
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder')
 
 if not DEBUG: 
     STATIC_ROOT = '/var/www/static'
