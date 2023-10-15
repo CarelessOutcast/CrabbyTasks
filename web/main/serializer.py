5b0f75dbab3ce5be30d82d334dbba6bc673d0837
CarelessOutcast <tapiacarlos32@gmail.com>
2023-10-15 18:47:36 -0500

    Remove web folder

--- web/main/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import * 
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = task_model
-        fields = ['task_id', 'complete', 'task', 'deadline']
-
