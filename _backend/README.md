251538b9da0eec8a723a1417ab22d646f10a8048
CarelessOutcast <tapiacarlos32@gmail.com>
2023-10-15 21:04:00 -0500

    Remove Docker scripts (for now)

---
+++ _backend/README.md
@@ -0,0 +1,8 @@
+#Django Backend Server
+
+#crabbytasks app holds general config
+
+#main app holds rest_framework endpoints
+
+
+
--- _backend/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"tapiacarlos32@gmail.com"}
-cd /app/
-
-/opt/env/bin/python manage.py migrate --noinput 
-/opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
-
-
