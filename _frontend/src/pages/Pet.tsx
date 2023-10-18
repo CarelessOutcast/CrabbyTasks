52ac8b5889f43a9c9745babba582e7fe6d561121
Nick Dilallo <nickd5465@gmail.com>
2023-10-17 19:51:48 -0500

    Sidebar renaming; Dm's removed; Calendar rename TaskManager; Search bar; Pet page

---
+++ _frontend/src/pages/Pet.tsx
@@ -0,0 +1,11 @@
+import Breadcrumb from '../components/Breadcrumb';
+
+const Pet = () => {
+    return (
+        <>
+            <Breadcrumb pageName="Pet" /> {/*Title that goes above content */}
+        </>
+    );
+};
+
+export default Pet;
