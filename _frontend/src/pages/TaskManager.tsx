0a65a5ef1e7a2d38eed1e7d759968d2916e2da0f
Nick Dilallo <nickd5465@gmail.com>
2023-10-19 19:43:24 -0500

    Tasklist menu popup created for one date

--- _frontend/src/pages/TaskManager.tsx
+++ _frontend/src/pages/TaskManager.tsx
@@ -1,7 +1,6 @@
-import React, { SetStateAction, useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import Breadcrumb from '../components/Breadcrumb';
-import ReactDOM from 'react-dom';
-
+import TaskList from '../components/TaskList';
 
 
 
@@ -15,60 +14,12 @@ const TaskManager = () => {
 
   }
 
-  useEffect(() => {
-    // This function will be triggered only when taskView changes
-    // Code to make on screen task menu appear goes here:
-    
-  }, [taskView]);
-
   const openTasks = (date: string) => {
     setTaskView(true);
-    //Retrieve tasks for inputted date code here? or in reaction to changing taskview:
+    //Retrieve tasks for inputted date code here?
 
   }
 
-  const TaskList = () => {
-    return(
-      <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 transition-opacity">
-              <div className="absolute inset-0 bg-gray-500 opacity-75" onClick={() => setTaskView(!taskView)}></div>
-            </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
-            <div
-              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-            >
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                      Modal Title
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm leading-5 text-gray-500">This is the content of the modal.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                    onClick={() => setTaskView(!taskView)}
-                  >
-                    Close
-                  </button>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-    )
-  }
 
   const MonthCalendar = () => {
     return (
@@ -469,8 +420,11 @@ const TaskManager = () => {
       {calendarView === "Month" && (<MonthCalendar />)}
       {calendarView === "Week" && (<WeekCalendar />)}
       {calendarView === "Day" && (<DayCalendar />)}
+      {/* {taskView && (
+        openTaskList()
+      )} */}
       {taskView && (
-        <TaskList/>
+        <TaskList setTaskView={setTaskView}/>
       )}
       {/* <!-- ====== Calendar Section End ====== --> */}
     </>
